bb0949b9d405f1aa22698e37ad6e4a94cedf42a2
N <cubingniles@gmail.com>
2022-01-14 22:13:21 +0530

    add

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-    res.send('hello');
+    res.status(200).send('hello');
 })
 
 app.listen(port, () => console.log('server started'))
